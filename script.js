d608113c995a819ebb114c78a35fbf82cb996389
Julie Ann <julieanniwinski@gmail.com>
2020-11-20 18:47:49 -0500

    Created function to make Question 1 appear

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 
+
+
+var question1 = document.getElementById("question1");
+var startButton = document.getElementById("startButton");
+var startPage = document.getElementById("startPage");
+
+$(startButton).on("click", function(){
+
+    startPage.style.display = "none"
+    question1.style.display = "block"
+});
+
